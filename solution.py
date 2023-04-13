a95704fe7b33b8bafb6ccaa505356b6b93939259
IronLetch <78549098+IronLetch@users.noreply.github.com>
2023-04-13 21:25:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import mannwhitneyu
 
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +9,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    U1, p = mannwhitneyu(males, females, method="exact")
+    alpha = 0.03
+    return (alpha>p)
